3f3e7c12c4e5e32c6a9e715b4df3904bd316a5f1
salehsami <salehsami017@gmail.com>
2021-08-05 16:00:10 +0500

    First commit

---
+++ Day 1 - Working with Variables in Python to Manage Data/Band Name Generator.py
@@ -0,0 +1,5 @@
+print("\t\t\t\tWelcome to the Band Name Generator")
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print(f"Your band name could be {city}-{pet}\n")
+print("Your band name could be "+ city +" "+ pet)
---
+++ Day 1 - Working with Variables in Python to Manage Data/len_str.py
@@ -0,0 +1,4 @@
+print(len(input("Enter your name ")))
+length = len(input("What is your name"))
+print(length)
+
