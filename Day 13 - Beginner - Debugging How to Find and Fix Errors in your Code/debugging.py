5308f91ed1b434a0abbe0d7d42855a001e1a3245
salehsami <67518300+salehsami@users.noreply.github.com>
2021-08-23 00:10:23 +0500

    Update debugging.py

--- Day 13 - Beginner - Debugging How to Find and Fix Errors in your Code/debugging.py
+++ Day 13 - Beginner - Debugging How to Find and Fix Errors in your Code/debugging.py
@@ -32,3 +32,12 @@
 # print(word_per_page)
 # total_words = pages * word_per_page
 # print(total_words)
+
+# Use a Debugger
+# def mutate(a_list):
+#     b_list = []
+#     for item in a_list:
+#         new_item = item * 2
+#         b_list.append(new_item)
+#     print(b_list)
+# mutate([1, 2, 3, 5, 8, 13])
