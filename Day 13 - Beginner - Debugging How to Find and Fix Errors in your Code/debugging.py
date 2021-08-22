08f002373421793119046c9e88c9d4aea0805051
salehsami <67518300+salehsami@users.noreply.github.com>
2021-08-22 23:57:56 +0500

    Add files via upload

---
+++ Day 13 - Beginner - Debugging How to Find and Fix Errors in your Code/debugging.py
@@ -0,0 +1,34 @@
+# Debugging the problems
+# def my_function():
+#     for i in range(1, 21):
+#         if i == 20:
+#             print("You got it")
+# my_function()
+
+# Reproduce the bug
+# import random
+# dice_imgs = ["1", "2", "3", "4", "5"]
+# dice_num = random.randint(0, 5)
+# print(dice_imgs[dice_num])
+
+# Play Computer
+# year = int(input("What's your year of birth: "))
+# if 1980 < year < 1994:
+#     print("You are a millennial")
+# elif year >= 1994:
+#     print("You are Gen Z.")
+
+# Fix the errors
+# age = int(input("How old are you? "))
+# if age > 18:
+#     print(f"You can drive at age {age}.")
+
+# Print is your friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page = int(input("Number of words per page: "))
+# print(pages)
+# print(word_per_page)
+# total_words = pages * word_per_page
+# print(total_words)
