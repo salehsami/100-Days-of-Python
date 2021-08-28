4de07c56babdc9c7ddf692b9cc1465c21026fe61
salehsami <67518300+salehsami@users.noreply.github.com>
2021-08-28 23:39:53 +0500

    Add files via upload

---
+++ Day 16  - Intermediate - Object Oriented Programming (OOP)/Turtle_forwarding/OOP(turtle_graphics).py
@@ -0,0 +1,13 @@
+import turtle
+# from turtle import Turtle, Screen
+timmy = turtle.Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.forward(100)
+# timmy.left(23)
+# timmy.forward(100)
+# timmy.left(23)
+timmy.color("red")
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
